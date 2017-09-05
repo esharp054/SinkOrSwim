6a854948798fdbba79af2bff642b22fe9b5a1a96
Elena Sharp <eesharp@smu.edu>
2017-09-04 20:16:29 -0500

    New Project Creation

---
+++ SinkOrSwim/SinkOrSwim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinkOrSwim
+//
+//  Created by Elena Sharp on 9/4/17.
+//  Copyright © 2017 Elena Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SinkOrSwim/SinkOrSwim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SinkOrSwim
+//
+//  Created by Elena Sharp on 9/4/17.
+//  Copyright © 2017 Elena Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
